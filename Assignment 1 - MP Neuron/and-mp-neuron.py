6015016b483669f07e6086e1fd7fbc3fa04d3dc3
Rishit Gautam <rishit.gautam2005@gmail.com>
2026-01-08 20:01:45 +0530

    Assignment 1

---
+++ Assignment 1 - MP Neuron/and-mp-neuron.py
@@ -0,0 +1,19 @@
+try: 
+    x1 = int(input("Enter x1: "))
+    x2 = int(input("Enter x2: "))
+    if x1 not in [0, 1] or x2 not in [0, 1]:
+        raise ValueError
+    
+    gx = x1 + x2
+    threshold = 2
+
+    if gx >= threshold:
+        y = 1
+    else:
+        y = 0
+
+    print("y =", y)
+    
+
+except ValueError:
+    print("Please enter binary inputs.")
